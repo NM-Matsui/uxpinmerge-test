0df16243918ec72d8485d83a12f4dc48b6cefd72
yuta.matsui <yuta.matsui@neuromagic.com>
2021-11-29 12:46:12 +0900

    [update] : ファイルアップ

---
+++ .uxpin-merge/components.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Button } from '../stories/Button';
+import { Header } from '../stories/Header';
+export {
+  Button,
+  Header,
+  React,
+  ReactDOM,
+};
